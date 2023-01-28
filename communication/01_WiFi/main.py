cb136e1bef81402982c18eca939f6bb229656716
lichuanqi <lc@dlc618.com>
2023-01-28 19:00:41 +0800

    修改：将无线名称和密码设置为全局参数

--- communication/01_WiFi/main.py
+++ communication/01_WiFi/main.py
@@ -1,20 +1,27 @@
 '''
-实验名称：连接无线路由器
-版本：v1.0
-日期：2019.8
-作者：01Studio
-说明：编程实现连接路由器，将IP地址等相关信息通过OLED显示（只支持2.4G网络）。
+连接wifi
+实现连接路由器，将IP地址等相关信息通过OLED显示（只支持2.4G网络）。
 '''
 import network,time
 from machine import I2C,Pin
 from ssd1306 import SSD1306_I2C
 
+
+# 参数
+WIFI_NAME = '601'
+WIFI_PWD = '1024601666'
+
 #初始化相关模块
 i2c = I2C(sda=Pin(13), scl=Pin(14))
 oled = SSD1306_I2C(128, 64, i2c, addr=0x3c)
 
-#WIFI连接函数
+
 def WIFI_Connect():
+    """WIFI连接函数
+    蓝灯闪烁-正在尝试连接
+    蓝灯常量-连接成功
+    蓝灯常闭-连接超时
+    """
 
     WIFI_LED=Pin(2, Pin.OUT) #初始化WIFI指示灯
 
@@ -23,30 +30,29 @@ def WIFI_Connect():
     start_time=time.time()              #记录时间做超时判断
 
     if not wlan.isconnected():
+        wlan.connect(WIFI_NAME, WIFI_PWD)
         print('connecting to network...')
-        wlan.connect('01Studio', '88888888') #输入WIFI账号密码
 
         while not wlan.isconnected():
-
-            #LED闪烁提示
+            # LED闪烁提示
             WIFI_LED.value(1)
             time.sleep_ms(300)
             WIFI_LED.value(0)
             time.sleep_ms(300)
 
-            #超时判断,15秒没连接成功判定为超时
+            # 超时判断,15秒没连接成功判定为超时
             if time.time()-start_time > 15 :
                 print('WIFI Connected Timeout!')
                 break
 
     if wlan.isconnected():
-        #LED点亮
+        # 打印网络信息
+        print('network information:', wlan.ifconfig())
+        
+        # LED点亮
         WIFI_LED.value(1)
 
-        #串口打印信息
-        print('network information:', wlan.ifconfig())
-
-        #OLED数据显示
+        # OLED数据显示
         oled.fill(0)   #清屏背景黑色
         oled.text('IP/Subnet/GW:',0,0)
         oled.text(wlan.ifconfig()[0], 0, 20)
@@ -54,5 +60,6 @@ def WIFI_Connect():
         oled.text(wlan.ifconfig()[2],0,56)
         oled.show()
 
-#执行WIFI连接函数
-WIFI_Connect()
+
+if __name__ == '__main__':
+    WIFI_Connect()
