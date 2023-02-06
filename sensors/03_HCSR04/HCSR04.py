04db41a4272581a47a65100e241cccf8fb1c87a0
lichuanqi <lc@dlc618.com>
2023-02-06 12:12:51 +0800

    修改：HCSR04超声测距波传感器

--- sensors/03_HCSR04/HCSR04.py
+++ sensors/03_HCSR04/HCSR04.py
@@ -1,5 +1,24 @@
+"""
+HCSR04 超声波传感器
 
+介绍
+HC-SR04超声波距离传感器的核心是两个超声波传感器。
+一个用作发射器，将电信号转换为40 KHz超声波脉冲。接收器监听发射的脉冲。
+如果接收到它们，它将产生一个输出脉冲，其宽度可用于确定脉冲传播的距离。
+
+参数
+Operating Voltage工作电压	直流5V
+Operating Current工作电流	15毫安
+Operating Frequency运行频率	40K赫兹
+Max Range最大范围	4m
+Min Range最小范围	2厘米
+Ranging Accuracy测距精度	3毫米
+Measuring Angle测量角度	15度
+Trigger Input Signal触发输入信号	10µS TTL脉冲
+Dimension尺寸	45 x 20 x 15毫米
+"""
 from time import sleep_us,ticks_us,sleep
+
 
 class HCSR04():
     def __init__(self,trig,echo):
--- sensors/03_HCSR04/main.py
+++ sensors/03_HCSR04/main.py
@@ -1,40 +1,32 @@
 '''
-实验名称：超声波传感器
-版本：v1.0
-日期：2019.8
-作者：01Studio 【www.01Studio.org】
-说明：通过超声波传感器测距，并在OLED上显示。
+超声波传感器测距，并在OLED上显示。
 '''
-
-from HCSR04 import HCSR04     #子文件夹下的调用方式
 from machine import Pin,I2C,Timer
 from ssd1306 import SSD1306_I2C
+from hcsr04 import HCSR04
 
-#初始化OLED
-i2c = I2C(sda=Pin(13), scl=Pin(14))
+# OLED模块
+i2c = I2C(sda=Pin(19), scl=Pin(21))
 oled = SSD1306_I2C(128, 64, i2c, addr=0x3c)
 
-#初始化接口 trig=17,echo=16
-trig = Pin(17,Pin.OUT)
-echo = Pin(16,Pin.IN)
-HC=HCSR04(trig,echo)
+# HCSR04模块
+HC=HCSR04(trig=Pin(32,Pin.OUT),echo=Pin(33,Pin.IN))
 
 #中断回调函数
 def fun(tim):
 
-    oled.fill(0)  # 清屏,背景黑色
-    oled.text('01Studio', 0, 0)
-    oled.text('Distance test:', 0, 15)
-
-    Distance = HC.getDistance() #测量距离
+    # 清屏,背景黑色
+    oled.fill(0) 
+    oled.text('Distance test:', 0, 0)
+    
+    # 测量距离
+    Distance = HC.getDistance()
+    print(str(Distance)+' CM')
 
     # OLED显示距离
-    oled.text(str(Distance) + ' CM', 0, 35)
-
+    oled.text(str(Distance) + ' CM', 0, 20)
     oled.show()
 
-    #串口打印
-    print(str(Distance)+' CM')
 
 #开启RTOS定时器
 tim = Timer(-1)
