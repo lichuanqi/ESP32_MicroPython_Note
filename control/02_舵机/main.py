795e2e838ad6e220e55f8dcc9a6b0b1d451bb1d6
lichuanqi <lc@dlc618.com>
2023-02-10 21:10:51 +0800

    修改：SG90

--- control/02_舵机/main.py
+++ control/02_舵机/main.py
@@ -1,42 +1,23 @@
 '''
-实验名称：舵机控制
-版本：v1.0
-日期：2019.8
-作者：01Studio 【www.01Studio.org】
-说明：通过编程控制舵机旋转到不同角度
-'''
+控制舵机SG90
 
+180度舵机：angle:-90至90 表示相应的角度
+360连续旋转度舵机：angle:-90至90 旋转方向和速度值。
+
+接线
+红色-VCC
+棕色-GND
+橙色-信号线
+'''
 from machine import Pin, PWM
 import time
 
-S1 = PWM(Pin(18), freq=50, duty=0) # Servo1的引脚是18
+S1 = PWM(Pin(13), freq=50, duty=0)
 
-'''
-说明：舵机控制函数
-功能：180度舵机：angle:-90至90 表示相应的角度
-     360连续旋转度舵机：angle:-90至90 旋转方向和速度值。
-'''
-def Servo(servo,angle):
+
+def Servo(servo, angle):
     S1.duty(int(((angle+90)*2/180+0.5)/20*1023))
 
 #-90度
-Servo(S1,-90)
-time.sleep(1)
-
-#-90度
-Servo(S1,-45)
-time.sleep(1)
-
-#-90度
 Servo(S1,0)
-time.sleep(1)
-
-#-90度
-Servo(S1,45)
-time.sleep(1)
-
-#-90度
-Servo(S1,90)
-time.sleep(1)
-
-
+time.sleep(2)
